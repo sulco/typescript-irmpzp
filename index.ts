f3563a6e38a6c61b2fb599aea426707d0526806d
sulco <tomek.sulkowski@gmail.com>
2022-06-21 17:29:49 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@
 import './style.css';
 import './tree.element';
 
-// Write TypeScript code!
+// Write TypeScript code!!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1><tree-element></tree-element>`;
